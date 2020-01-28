85050225301abcbf925861c4d173ad31d7c00754
Cara Townsend <cara.townsend@ten10.com>
2020-01-28 15:11:50 +0000

    follows frameworkium/selenium support page object model

--- src/test/java/pageModels/HomePage.java
+++ src/test/java/pageModels/HomePage.java
@@ -1,10 +1,17 @@
 package pageModels;
 
-public class HomePage implements Page {
+import com.frameworkium.core.ui.pages.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+public class HomePage extends BasePage<HomePage> implements Page {
     public static final String URL = "http://";
+    @FindBy(css="#q")
+    WebElement searchBox;
 
     @Override
     public void navigate() {
-
+        get(URL);
     }
 }
